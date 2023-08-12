376327ca792897a1158cb7f5d5664255767900fa
A-Walrus <ofek.r.guy@gmail.com>
2023-08-12 13:07:56 +0300

    Day 7

--- Solutions/Day07.hs
+++ Solutions/Day07.hs
@@ -25,14 +25,34 @@ insertFile :: File -> Path -> File -> File
 insertFile (Dir map) [name] file = Dir (Map.insert name file map)
 insertFile (Dir map) (name : xs) file = Dir (Map.update (\dir -> Just $ insertFile dir xs file) name map)
 
-
 part1 :: Parsed -> Int
-part1 = undefined
+part1 = sumAtMost
 
 part2 :: Parsed -> Int
-part2 = undefined
-
+part2 dir = smallestAbove needToFree dir
+  where
+    needToFree = size dir - 40000000
 
 type Path = [String]
 
 data File = Dir (Map.Map String File) | Regular Int deriving (Show)
+
+size :: File -> Int
+size (Regular x) = x
+size (Dir map) = sum $ Map.map size map
+
+sumAtMost :: File -> Int
+sumAtMost (Regular _) = 0
+sumAtMost dir@(Dir children) =
+  childSolve
+    + if size dir > 100000
+      then 0
+      else size dir
+  where
+    childSolve = sum $ Map.map sumAtMost children
+
+smallestAbove :: Int -> File -> Int
+smallestAbove n (Regular _) = maxBound
+smallestAbove n dir@(Dir children)
+  | size dir >= n = min (size dir) (minimum (map (smallestAbove n) (Map.elems children)))
+  | otherwise = maxBound
